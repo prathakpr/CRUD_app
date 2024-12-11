96d59dd5a0de30a29639f65c2aa017dfa2e475f3
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-11 23:20:14 +0530

    added movies controller's endpoints and created service files

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    @Post()
+    create() {
+    return "create a new movie endpoint";
+    }
+    @Get()
+    findAll() {
+    return "find all movie endpoint";
+    }
+    @Get(":id")
+    findOne() {
+    return "fetch movie on the based on id";
+    }
+    @Put(":id")
+    update() {
+    return "update movie on the based on id";
+    }
+    @Delete(":id")
+    delete() {
+    return "delete a movie on the based on id";
+    }
+    
+}
