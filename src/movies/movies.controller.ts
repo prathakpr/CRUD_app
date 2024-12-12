524b1e4cfa8ebfbda917a8ee3239ac3a7aaaa888
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-12 14:26:37 +0530

    Created 'movie' repository methods for CRUD operations within our local DB

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,30 +1,42 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity'; // Import the Movie entity
 
 @Controller('movies')
 export class MoviesController {
     constructor(private moviesService: MoviesService){}
-    @Post()
-    create() {
-        return this.moviesService.create("Bahubali");
-    //return "create a new movie endpoint";
-    }
-    @Get()
-    findAll() {
-        return this.moviesService.findAll();
-    //return "find all movie endpoint";
-    }
-    @Get(":id")
-    findOne() {
-    return "fetch movie on the based on id";
-    }
-    @Put(":id")
-    update() {
-    return "update movie on the based on id";
-    }
-    @Delete(":id")
-    delete() {
-    return "delete a movie on the based on id";
-    }
-    
+
+    // Create a movie
+  @Post()
+  async create(@Body() movie: Movie): Promise<Movie> {
+    return this.moviesService.create(movie);
+  }
+
+  // Get all movies
+  @Get()
+  async findAll(): Promise<Movie[]> {
+    return this.moviesService.findAll();
+  }
+
+  // Get a movie by id
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Movie> {
+    return this.moviesService.findOne(id);
+  }
+
+  // Update a movie
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() movie: Movie,
+  ): Promise<Movie> {
+    return this.moviesService.update(id, movie);
+  }
+
+  // Delete a movie
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.moviesService.remove(id);
+  }
+  
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -11,7 +11,35 @@ export class MoviesService {
         @InjectRepository(Movie) 
         private movieRepository: Repository<Movie>, //we will use movieRepository for our CRUD operations
       ) {}
-   // local array
+
+       // Create a movie
+  async create(movie: Partial<Movie>): Promise<Movie> {
+    const newMovie = this.movieRepository.create(movie); // Create a new movie entity
+    return await this.movieRepository.save(newMovie); // Save to DB
+  }
+
+  // Get all movies
+  async findAll(): Promise<Movie[]> {
+    return this.movieRepository.find(); // Get all movies
+  }
+
+  // Get a movie by id
+  async findOne(id: number): Promise<Movie> {
+    return this.movieRepository.findOne({ where: { id } }); // Get movie by id
+  }
+
+  // Update a movie
+  async update(id: number, movie: Partial<Movie>): Promise<Movie> {
+    await this.movieRepository.update(id, movie); // Update movie by id
+    return this.findOne(id); // Return the updated movie
+  }
+
+  // Delete a movie
+  async remove(id: number): Promise<void> {
+    await this.movieRepository.delete(id); // Delete movie by id
+  }
+
+   /* local array
     private readonly movies = [];
     create(movie : string) {
         // Save the song in the database
@@ -22,5 +50,5 @@ export class MoviesService {
         // fetch the songs from the db
         throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
         return this.movies;
-    }
+    } */
 }
