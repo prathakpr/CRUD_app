c7c02d64bdbd57695937eeb53854ae984bd8598a
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-12 12:50:31 +0530

    installed typeORM for postgres and setup connection with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'movieDB',
+      entities: [],
+      synchronize: true,
+      })],
   controllers: [AppController],
   providers: [AppService],
 })
