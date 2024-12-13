e3b8e911d86d3664afcbacc6350c428e4e8bee09
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-13 13:56:55 +0530

    added controller and service of song for the CRUD operations through MongoDB

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { SongsService } from './songs.service';
+import { Song } from './schemas/song.schema';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+    constructor(private readonly songsService: SongsService) {}
+
+    @Post()
+    async create(@Body() song: Song): Promise<Song> {
+      return this.songsService.create(song);
+    }
+  
+    @Get()
+    async findAll(): Promise<Song[]> {
+      return this.songsService.findAll();
+    }
+  
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Song> {
+      return this.songsService.findOne(id);
+    }
+  
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() song: Song): Promise<Song> {
+      return this.songsService.update(id, song);
+    }
+  
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<void> {
+      return this.songsService.remove(id);
+    }
+}
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Song } from './schemas/song.schema';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+    constructor(
+        @InjectModel(Song.name) private readonly songModel: Model<Song>,
+      ) {}
+    
+      // Create a song
+      async create(song: Partial<Song>): Promise<Song> {
+        const newSong = new this.songModel(song);
+        return newSong.save();
+      }
+    
+      // Get all songs
+      async findAll(): Promise<Song[]> {
+        return this.songModel.find().exec();
+      }
+    
+      // Get a song by id
+      async findOne(id: string): Promise<Song> {
+        return this.songModel.findById(id).exec();
+      }
+    
+      // Update a song
+      async update(id: string, song: Partial<Song>): Promise<Song> {
+        return this.songModel.findByIdAndUpdate(id, song, { new: true }).exec();
+      }
+    
+      // Delete a song
+      async remove(id: string): Promise<void> {
+        await this.songModel.findByIdAndDelete(id).exec();
+      }
+}
